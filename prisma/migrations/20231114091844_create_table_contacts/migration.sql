fd466e0f0f1e1923ea62fddf08acc527242fc21b
Adiva <prayoga.adiva@gmail.com>
2023-11-14 17:25:24 +0800

    fix schema

--- prisma/migrations/20231114091844_create_table_contacts/migration.sql
+++ prisma/migrations/20231114091844_create_table_contacts/migration.sql
@@ -1,8 +1,8 @@
 -- CreateTable
 CREATE TABLE `contacts` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `firstName` VARCHAR(100) NOT NULL,
-    `lastName` VARCHAR(100) NULL,
+    `first_name` VARCHAR(100) NOT NULL,
+    `last_name` VARCHAR(100) NULL,
     `email` VARCHAR(200) NULL,
     `phone` VARCHAR(20) NULL,
     `username` VARCHAR(100) NOT NULL,
